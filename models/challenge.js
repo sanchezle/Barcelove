5c8408a2d2cdb52b1e219c5a54aaf7b4b312e9da
sanchezle <sanchezle21@gmail.com>
2023-07-22 17:18:45 +0200

    port enviroment variable and 3000 as default

--- models/challenge.js
+++ models/challenge.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const LocationSchema = new Schema({
+    name: String,  // name of the location
+    score: Number,  // score for this location
+    confirmed: { type: Boolean, default: false }  // whether the user has been to this location
+});
+
 const ChallengeSchema = new Schema({
-    completed: { type: Boolean, default: false },
-    currentScore: { type: Number, default: 0 },
-    locations: [
-        { name: 'Location 1', confirmed: false, score: 50 },
-        { name: 'Location 2', confirmed: false, score: 50 },
-        { name: 'Location 3', confirmed: false, score: 50 },
-        { name: 'Location 4', confirmed: false, score: 50 },
-        { name: 'Arc of Triumph', confirmed: false, score: 50 },
-    ]
+    name: String,  // name of the challenge
+    locations: [LocationSchema],  // array of locations
+    completed: { type: Boolean, default: false },  // whether the challenge is completed
+    currentScore: { type: Number, default: 0 },  // current accumulated score
+    totalScore: Number  // total score for the challenge
 });
 
 module.exports = mongoose.model('Challenge', ChallengeSchema);

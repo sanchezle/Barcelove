da80e1d4e0f426e5f48402126d617a5a7af697d7
sanchezle <sanchezle21@gmail.com>
2023-07-25 00:44:20 +0200

    htmls

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const Challeng = require('./models/challenge');
 
+const MongoStore = require('connect-mongo');
+
 const fetch = require('node-fetch');
 
 
 const User = require('./models/user');
 const bcrypt = require('bcrypt');
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
 
 const saltRounds = 10;
 const salt = bcrypt.genSaltSync(saltRounds);
@@ -27,25 +29,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-app.get('/', (req, res) =>{
-    res.sendFile(__dirname + '/public/index.html');
-});
+
+const register = require('./routes/register');
+const login = require('./routes/login');
+
+app.use('/register', register);
+app.use('/login', login);
 
 
-// user registration
-app.post('/register', (req, res) => {
-    // registration logic here
-  });
-  
-  // user login
-  app.post('/login', (req, res) => {
-    // login logic here
-  });
+
   
   // fetch user profile
   app.get('/user/:userId', (req, res) => {
     // fetch user profile logic here
   });
   
+  app.use(session({
+    secret: 'SomeSecretKey',
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongooseConnection: mongoose.connection }),
+}));
 
 app.listen(port, () => console.log(`app listening at http://localhost:${port}`));
--- routes/register.js
+++ routes/register.js
@@ -1,26 +1,42 @@
+// register.js
+
 const express = require('express');
-const app = express();
 const router = express.Router();
+const User = require('./models/User'); // Import the User model
 
+// Route to render the registration form
+router.get('/', (req, res) => {
+  res.sendFile(__dirname + '/path/to/register.html'); // Adjust the path accordingly
+});
 
-// GET method route
-router.get('/', function (req, res) {
-    res.send(__dirname + "templates/index.html"_);
-    });
+// Route to handle form submission
+router.post('/', async (req, res) => {
+  const { username, email, password, confirmPassword } = req.body;
 
-// POST method route
-router.post('/', function (req, res) {
-    res.send('POST request to the homepage');
-    });
+  // Validation logic
+  if (!username || !email || !password || !confirmPassword) {
+    return res.status(400).send('All fields are required.');
+  }
 
-// PUT method route
-router.put('/', function (req, res) {
-    res.send('PUT request to the homepage');
+  if (password !== confirmPassword) {
+    return res.status(400).send('Passwords do not match.');
+  }
+
+  // Check if the username or email already exists in the database
+  try {
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+
+    if (existingUser) {
+      return res.status(400).send('Username or email already exists.');
     }
-);
 
-// DELETE method route
-router.delete('/', function (req, res) {
-    res.send('DELETE request to the homepage');
-    }
-);
+    // Create a new user in the database
+    const newUser = new User({ username, email, password });
+    await newUser.save();
+    res.send('Registration successful!'); // You can redirect to a success page or perform further actions here.
+  } catch (error) {
+    res.status(500).send('Error registering user.'); // Handle the error gracefully.
+  }
+});
+
+module.exports = router;

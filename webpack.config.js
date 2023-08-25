e2bd22cbfc762e95d748853936c5585783bee8db
sanchezle <sanchezle21@gmail.com>
2023-08-25 12:38:17 +0200

    react files and config

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development', // or 'production'
+  entry: './src/index.jsx', // Entry point to your application
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Output directory
+    filename: 'main.js' // Output file
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'), // Directory to serve files from
+    port: 3000 // Port number for development server
+  }
+};

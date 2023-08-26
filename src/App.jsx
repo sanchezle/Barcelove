e601ae5c40f8ce025a9cab11c66f4783ebf02dfb
sanchezle <sanchezle21@gmail.com>
2023-08-26 18:48:11 +0200

    webpack re-build

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import UserRouter from './userRoute';
+
 import UserProfile from './components/UserProfile'; // Assuming UserProfile.jsx is in the /components directory
 
 const App = () => {

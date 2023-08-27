ecbc020709d143d6e130e7dceaed38c3f4126710
sanchezle <sanchezle21@gmail.com>
2023-08-27 20:09:56 +0200

    trying App with UserProfile component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import UserProfile from './components/UserProfile'; // Assuming UserProfile.jsx is in the /components directory
 
-const App = () => (
-  <div>
-    <h1>Hello, React! BACK TO THIS</h1>
-    <div>
-      <user/>
-    </div>
-  </div>
-);
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        {/* other routes */}
+        <Route path="/profile" component={UserProfile} /> 
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;

4dc617eb32c2bd09dfe1e1e92d7c141c9e6491f7
sanchezle <sanchezle21@gmail.com>
2023-08-28 19:28:54 +0200

    new index.jsx and app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import UserProfile from './components/UserProfile'; // Assuming UserProfile.jsx is in the /components directory
-
-const App = () => {
-  return (
-    <Router>
-      <Routes>
-        {/* other routes */}
-        <Route path="/profile" component={UserProfile} /> 
-      </Routes>
-    </Router>
-  );
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
 };
 
-export default App;
+export default function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}

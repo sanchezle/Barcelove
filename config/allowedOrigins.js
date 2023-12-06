a4ed58d6faedf7356483382ee5d22e28e5ac7200
sanchezle <sanchezle21@gmail.com>
2023-12-07 00:35:26 +0100

    add netlify origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:3002',
     'http://localhost:3003', 
-    'https://barcelove-client-69d69fffaff4.herokuapp.com/' 
+    'https://barcelove-client-69d69fffaff4.herokuapp.com/' ,
+    'https://main--mellow-mochi-d0d65a.netlify.app/'
 ]
 
 module.exports = allowedOrigins;

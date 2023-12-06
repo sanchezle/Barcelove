3707bae9f25ea5132bc795f275f6f62ff4b59406
sanchezle <sanchezle21@gmail.com>
2023-12-06 13:46:03 +0100

    allowing barcelvo-client url origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:3002',
-    'http://localhost:3003',  
+    'http://localhost:3003', 
+    'https://barcelove-client-69d69fffaff4.herokuapp.com/' 
 ]
 
 module.exports = allowedOrigins;

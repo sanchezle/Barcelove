d3747e372b86ecc33bec315548b05208a43ce8ff
sanchezle <sanchezle21@gmail.com>
2023-12-07 12:09:51 +0100

    allowedOrigins fixed

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,6 +4,6 @@ const allowedOrigins = [
     'http://localhost:3003', 
     'https://barcelove-client-69d69fffaff4.herokuapp.com/' ,
     'https://main--mellow-mochi-d0d65a.netlify.app'
-]
+];
 
 module.exports = allowedOrigins;

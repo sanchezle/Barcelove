e970233feacef2d5ac19d711927ce4d1451e0a15
sanchezle <sanchezle21@gmail.com>
2023-12-07 14:15:21 +0100

    allowedOrigins fixed this time yes yes

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     'http://localhost:3002',
-    'https://mellow-mochi-d0d65a.netlify.app/'
+    'https://mellow-mochi-d0d65a.netlify.app'
 ];
 
 module.exports = allowedOrigins;

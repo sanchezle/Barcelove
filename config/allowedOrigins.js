93a8e4380cff48e8b1a100a1a54f35e3de58d51d
sanchezle <sanchezle21@gmail.com>
2023-12-07 14:11:40 +0100

    change name in json file

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,5 @@
 const allowedOrigins = [
-    'http://localhost:3000',
     'http://localhost:3002',
-    'https://barcelove-client-69d69fffaff4.herokuapp.com/' ,
     'https://mellow-mochi-d0d65a.netlify.app/'
 ];
 
